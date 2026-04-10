5fc49146f543d67ca0ab564cd17669e3a084895b
Ujaan Das <ujaandas03@gmail.com>
2026-04-10 16:35:02 +0000

    wip lexer api

---
+++ lang/src/lexer/lexer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "token.hpp"
+
+namespace invariants::lexer {
+
+class Lexer {
+ private:
+  const std::string source;
+  std::vector<Token> tokens;
+  size_t start;
+  size_t curr;
+  size_t line;
+
+  void scanToken();
+  char advance();
+  void addToken(TokenType type);
+  void addToken(TokenType type, Literal literal);
+
+ public:
+  Lexer(std::string source);
+  std::vector<Token> scanTokens();
+};
+
+}  // namespace invariants::lexer
