86907fdecbab50429873b7c59c47ec07e021bb30
Ujaan Das <ujaandas03@gmail.com>
2026-04-23 22:23:32 +0000

    clean up interpo

--- lang/src/interp/interpreter.cpp
+++ lang/src/interp/interpreter.cpp
@@ -2,17 +2,12 @@
 #include "interpreter.hpp"
 
 #include <print>
-// #include <string_view>
 
 #include "lexer.hpp"
 
 namespace invariants::interpreter {
 
-// void Interpreter::report(std::size_t line, std::string_view where,
-//                          std::string_view msg) {
-//   std::println("[line %d] Error %s : %s", line, where, msg);
-//   hadErr = true;
-// }
+// TODO: implement and wire up centralized interp error reporting
 
 void Interpreter::run(std::string_view src) {
   lexer::Lexer lexer(src);
--- lang/src/interp/interpreter.hpp
+++ lang/src/interp/interpreter.hpp
@@ -1,15 +1,10 @@
 #pragma once
 
-#include <cstddef>
 #include <string_view>
 
 namespace invariants::interpreter {
 
 class Interpreter {
- private:
-  bool hadErr = false;
-  void report(std::size_t line, std::string_view where, std::string_view msg);
-
  public:
   void run(const std::string_view src);
 };
