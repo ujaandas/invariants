51de7b91b6820e2e190c488b6a81525c99db50da
Ujaan Das <ujaandas03@gmail.com>
2026-04-23 21:53:54 +0000

    add a bunch more lexer tests

--- lang/tests/lexer/lexer_test.cpp
+++ lang/tests/lexer/lexer_test.cpp
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -80,6 +81,10 @@ INSTANTIATE_TEST_SUITE_P(
                               {Token{TokenType::PLUS, "+", 1},
                                Token{TokenType::EOF_TOKEN, "", 1}},
                               "Plus"},
+                    TokenCase{"-",
+                              {Token{TokenType::MINUS, "-", 1},
+                               Token{TokenType::EOF_TOKEN, "", 1}},
+                              "Minus"},
                     TokenCase{"/",
                               {Token{TokenType::SLASH, "/", 1},
                                Token{TokenType::EOF_TOKEN, "", 1}},
@@ -91,7 +96,23 @@ INSTANTIATE_TEST_SUITE_P(
                     TokenCase{"%",
                               {Token{TokenType::PERCENTAGE, "%", 1},
                                Token{TokenType::EOF_TOKEN, "", 1}},
-                              "Percentage"}));
+                              "Percentage"},
+                    TokenCase{"!",
+                              {Token{TokenType::BANG, "!", 1},
+                               Token{TokenType::EOF_TOKEN, "", 1}},
+                              "Bang"},
+                    TokenCase{"=",
+                              {Token{TokenType::EQUAL, "=", 1},
+                               Token{TokenType::EOF_TOKEN, "", 1}},
+                              "Equal"},
+                    TokenCase{">",
+                              {Token{TokenType::GREATER, ">", 1},
+                               Token{TokenType::EOF_TOKEN, "", 1}},
+                              "Greater"},
+                    TokenCase{"<",
+                              {Token{TokenType::LESS, "<", 1},
+                               Token{TokenType::EOF_TOKEN, "", 1}},
+                              "Less"}));
 
 INSTANTIATE_TEST_SUITE_P(
     MultiChar, LexerScansTokenTest,
@@ -177,447 +198,141 @@ INSTANTIATE_TEST_SUITE_P(
                                Token{TokenType::EOF_TOKEN, "", 1}},
                               "Contains"}));
 
-// TEST(LexerTest, ScansSimplePunctuationSequence) {
-//   Lexer lexer("[]+;");
+TEST(LexerTest, IgnoresWhitespaceAndTracksNewlineForEof) {
+  Lexer lexer(" \t\r\n");
 
-//   const auto tokens = lexer.scanTokens();
+  const std::vector<Token> expected = {
+      Token{TokenType::EOF_TOKEN, "", 2},
+  };
 
-//   const std::vector<Token> expected = {
-//       Token(TokenType::LEFT_BRACKET, "[", 1),
-//       Token(TokenType::RIGHT_BRACKET, "]", 1),
-//       Token(TokenType::PLUS, "+", 1),
-//       Token(TokenType::SEMICOLON, ";", 1),
-//       Token(TokenType::EOF_TOKEN, "", 1),
-//   };
+  EXPECT_EQ(lexer.scanTokens(), expected);
+}
 
-//   EXPECT_EQ(tokens, expected);
-// }
+TEST(LexerTest, SkipsSingleLineComment) {
+  Lexer lexer("// comment only");
 
-// TEST(LexerTest, ThrowsOnInvalidToken) {
-//   Lexer lexer("@");
-//   EXPECT_THROW(lexer.scanTokens(), std::invalid_argument);
-// }
+  const std::vector<Token> expected = {
+      Token{TokenType::EOF_TOKEN, "", 1},
+  };
 
-// TEST(LexerTest, ScansSingleBang) {
-//   Lexer lexer("!");
+  EXPECT_EQ(lexer.scanTokens(), expected);
+}
 
-//   const auto tokens = lexer.scanTokens();
+TEST(LexerTest, ScansSlashAfterCommentLines) {
+  Lexer lexer("// first\n// second\n/");
 
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::BANG, "!", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansBangEqual) {
-//   Lexer lexer("!=");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::BANG_EQUAL, "!=", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansSingleEqual) {
-//   Lexer lexer("=");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::EQUAL, "=", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansEqualEqual) {
-//   Lexer lexer("==");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::EQUAL_EQUAL, "==", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansSingleGreater) {
-//   Lexer lexer(">");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::GREATER, ">", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansGreaterEqual) {
-//   Lexer lexer(">=");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::GREATER_EQUAL, ">=", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansSingleLess) {
-//   Lexer lexer("<");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LESS, "<", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansLessEqual) {
-//   Lexer lexer("<=");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LESS_EQUAL, "<=", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansSingleMinus) {
-//   Lexer lexer("-");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::MINUS, "-", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansArrow) {
-//   Lexer lexer("->");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::ARROW, "->", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansSingleDivide) {
-//   Lexer lexer("/");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::SLASH, "/", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansComment) {
-//   Lexer lexer("// this is a comment");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 1);
-//   EXPECT_EQ(tokens[0], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansIgnoresWhitespace) {
-//   Lexer lexer(" ");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 1);
-//   EXPECT_EQ(tokens[0], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansNewline) {
-//   Lexer lexer("\n");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 1);
-//   EXPECT_EQ(tokens[0], Token(TokenType::EOF_TOKEN, "", 2));
-// }
-
-// TEST(LexerTest, ScansMultipleCommentedLines) {
-//   Lexer lexer(
-//       "// this is a comment\n"
-//       "// this is another comment\n");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 1);
-//   EXPECT_EQ(tokens[0], Token(TokenType::EOF_TOKEN, "", 3));
-// }
-
-// TEST(LexerTest, ScansDivideAfterComments) {
-//   Lexer lexer(
-//       "// this is a comment\n"
-//       "// this is another comment\n"
-//       "/");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::SLASH, "/", 3));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 3));
-// }
-
-// TEST(LexerTest, ScansStringLiteral) {
-//   Lexer lexer("\"Hello, World!\"");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LIT_STRING, "\"Hello, World!\"",
-//                              "Hello, World!", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansIntegerLiteral) {
-//   Lexer lexer("123");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LIT_INTEGER, "123", 123, 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansDoubleLiteral) {
-//   Lexer lexer("123.456");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LIT_NUMBER, "123.456", 123.456,
-//   1)); EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// // TEST(LexerTest, LeadingDecimalIllegal) {
-// //   Lexer lexer(".456");
-
-// //   const auto tokens = lexer.scanTokens();
-
-// //   for (auto& token : tokens) {
-// //     std::cout << token.toString() << ",\n";
-// //   }
-
-// //   // .456 actually evaluates to tokens DOT, 456.000..., EOF
-// //   EXPECT_EQ(tokens.size(), 3);
-// //   EXPECT_EQ(tokens[0], Token(TokenType::LIT_NUMBER, ".456", .456, 1));
-// //   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// // }
-
-// TEST(LexerTest, ScansIdentifier) {
-//   Lexer lexer("foobar");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LIT_IDENTIFIER, "foobar", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansIdentifierPrependUnderscore) {
-//   Lexer lexer("_foobar");
-//   EXPECT_THROW(lexer.scanTokens(), std::invalid_argument);
-// }
-
-// TEST(LexerTest, ScansIdentifierAppendUnderscore) {
-//   Lexer lexer("foobar_");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LIT_IDENTIFIER, "foobar_", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansIdentifierMiddleUnderscore) {
-//   Lexer lexer("foo_bar");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LIT_IDENTIFIER, "foo_bar", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansSpecKeyword) {
-//   Lexer lexer("spec");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_SPEC, "spec", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansFieldKeyword) {
-//   Lexer lexer("field");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_FIELD, "field", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansCheckKeyword) {
-//   Lexer lexer("check");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_CHECK, "check", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansInvariantKeyword) {
-//   Lexer lexer("invariant");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_INVARIANT, "invariant", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansBooleanKeyword) {
-//   Lexer lexer("Boolean");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_BOOLEAN, "Boolean", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansArrayKeyword) {
-//   Lexer lexer("Array");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_ARRAY, "Array", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansNullKeyword) {
-//   Lexer lexer("Null");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_NULL, "Null", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansStringKeyword) {
-//   Lexer lexer("String");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_STRING, "String", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansNumberKeyword) {
-//   Lexer lexer("Number");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_NUMBER, "Number", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansIntegerKeyword) {
-//   Lexer lexer("Integer");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_INTEGER, "Integer", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansInKeyword) {
-//   Lexer lexer("IN");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_IN, "IN", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansNotInKeyword) {
-//   Lexer lexer("NIN");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_NOT_IN, "NIN", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansContainsKeyword) {
-//   Lexer lexer("NI");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_CONTAINS, "NI", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, DifferentiatesKeywordLiteralBooleanT) {
-//   Lexer lexer("Boolean true");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 3);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_BOOLEAN, "Boolean", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::LIT_BOOLEAN_T, "true", true, 1));
-//   EXPECT_EQ(tokens[2], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, DifferentiatesKeywordLiteralBooleanF) {
-//   Lexer lexer("Boolean false");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 3);
-//   EXPECT_EQ(tokens[0], Token(TokenType::KW_BOOLEAN, "Boolean", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::LIT_BOOLEAN_F, "false", false, 1));
-//   EXPECT_EQ(tokens[2], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansLiteralBooleanTrue) {
-//   Lexer lexer("true");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LIT_BOOLEAN_T, "true", true, 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansLiteralBooleanFalse) {
-//   Lexer lexer("false");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LIT_BOOLEAN_F, "false", false, 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
-
-// TEST(LexerTest, ScansLiteralNull) {
-//   Lexer lexer("null");
-
-//   const auto tokens = lexer.scanTokens();
-
-//   EXPECT_EQ(tokens.size(), 2);
-//   EXPECT_EQ(tokens[0], Token(TokenType::LIT_NULL, "null", 1));
-//   EXPECT_EQ(tokens[1], Token(TokenType::EOF_TOKEN, "", 1));
-// }
+  const std::vector<Token> expected = {
+      Token{TokenType::SLASH, "/", 3},
+      Token{TokenType::EOF_TOKEN, "", 3},
+  };
+
+  EXPECT_EQ(lexer.scanTokens(), expected);
+}
+
+TEST(LexerTest, ScansStringLiteralWithLiteralValue) {
+  Lexer lexer("\"Hello, world!\"");
+
+  const std::vector<Token> expected = {
+      Token{TokenType::LIT_STRING, "\"Hello, world!\"",
+            std::string("Hello, world!"), 1},
+      Token{TokenType::EOF_TOKEN, "", 1},
+  };
+
+  EXPECT_EQ(lexer.scanTokens(), expected);
+}
+
+TEST(LexerTest, ThrowsOnUnterminatedString) {
+  Lexer lexer("\"unterminated");
+
+  EXPECT_THROW(lexer.scanTokens(), std::range_error);
+}
+
+TEST(LexerTest, ScansIntegerAndNumberLiterals) {
+  Lexer lexer("123 45.5");
+
+  const std::vector<Token> expected = {
+      Token{TokenType::LIT_INTEGER, "123", 123, 1},
+      Token{TokenType::LIT_NUMBER, "45.5", 45.5, 1},
+      Token{TokenType::EOF_TOKEN, "", 1},
+  };
+
+  EXPECT_EQ(lexer.scanTokens(), expected);
+}
+
+TEST(LexerTest, TreatsLeadingDecimalAsDotThenInteger) {
+  Lexer lexer(".456");
+
+  const std::vector<Token> expected = {
+      Token{TokenType::DOT, ".", 1},
+      Token{TokenType::LIT_INTEGER, "456", 456, 1},
+      Token{TokenType::EOF_TOKEN, "", 1},
+  };
+
+  EXPECT_EQ(lexer.scanTokens(), expected);
+}
+
+TEST(LexerTest, ScansIdentifiersWithUnderscoresAfterFirstChar) {
+  Lexer lexer("foo_bar bar_2");
+
+  const std::vector<Token> expected = {
+      Token{TokenType::LIT_IDENTIFIER, "foo_bar", 1},
+      Token{TokenType::LIT_IDENTIFIER, "bar_2", 1},
+      Token{TokenType::EOF_TOKEN, "", 1},
+  };
+
+  EXPECT_EQ(lexer.scanTokens(), expected);
+}
+
+TEST(LexerTest, RejectsLeadingUnderscoreIdentifier) {
+  Lexer lexer("_bad");
+
+  EXPECT_THROW(lexer.scanTokens(), std::invalid_argument);
+}
+
+TEST(LexerTest, DifferentiatesKeywordAndIdentifierPrefixes) {
+  Lexer lexer("spec specX Boolean true false null");
+
+  const std::vector<Token> expected = {
+      Token{TokenType::KW_SPEC, "spec", 1},
+      Token{TokenType::LIT_IDENTIFIER, "specX", 1},
+      Token{TokenType::KW_BOOLEAN, "Boolean", 1},
+      Token{TokenType::LIT_BOOLEAN_T, "true", true, 1},
+      Token{TokenType::LIT_BOOLEAN_F, "false", false, 1},
+      Token{TokenType::LIT_NULL, "null", 1},
+      Token{TokenType::EOF_TOKEN, "", 1},
+  };
+
+  EXPECT_EQ(lexer.scanTokens(), expected);
+}
+
+TEST(LexerTest, ThrowsOnInvalidToken) {
+  Lexer lexer("@");
+
+  EXPECT_THROW(lexer.scanTokens(), std::invalid_argument);
+}
+
+TEST(LexerTest, ScansMixedProgramAndTracksTokenLines) {
+  Lexer lexer(
+      "spec User {\n"
+      "field age: Integer\n"
+      "check age >= 18\n"
+      "}");
+
+  const std::vector<Token> expected = {
+      Token{TokenType::KW_SPEC, "spec", 1},
+      Token{TokenType::LIT_IDENTIFIER, "User", 1},
+      Token{TokenType::LEFT_BRACE, "{", 1},
+      Token{TokenType::KW_FIELD, "field", 2},
+      Token{TokenType::LIT_IDENTIFIER, "age", 2},
+      Token{TokenType::COLON, ":", 2},
+      Token{TokenType::KW_INTEGER, "Integer", 2},
+      Token{TokenType::KW_CHECK, "check", 3},
+      Token{TokenType::LIT_IDENTIFIER, "age", 3},
+      Token{TokenType::GREATER_EQUAL, ">=", 3},
+      Token{TokenType::LIT_INTEGER, "18", 18, 3},
+      Token{TokenType::RIGHT_BRACE, "}", 4},
+      Token{TokenType::EOF_TOKEN, "", 4},
+  };
+
+  EXPECT_EQ(lexer.scanTokens(), expected);
+}
