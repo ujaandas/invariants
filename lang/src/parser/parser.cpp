065019486f1a1c021c5ca94c399d1aebfad0cbc8
Ujaan Das <ujaandas03@gmail.com>
2026-05-04 21:59:53 +0000

    wip add expr rules to parser

--- lang/src/parser/parser.cpp
+++ lang/src/parser/parser.cpp
@@ -1,5 +1,43 @@
 #include "parser.hpp"
 
+#include <memory>
+
+#include "expression.hpp"
+#include "token.hpp"
+
 using namespace invariants::parser;
+using invariants::ast::ExprPtr;
 
 Parser::Parser(const std::vector<lexer::Token>& tokens) : tokens(tokens) {}
+
+ExprPtr Parser::expression() { return implication(); }
+
+ExprPtr Parser::implication() {
+  ExprPtr left = disjunction();
+
+  if (match(lexer::TokenType::ARROW)) {
+    ExprPtr right = implication();
+
+    return std::make_unique<ast::Expr>(ast::BinaryExpr{
+        std::move(left), ast::BinaryOp::Imply, std::move(right)});
+  }
+
+  return left;
+}
+
+// ExprPtr Parser::disjunction() {
+//   ExprPtr left = conjunction();
+
+//   if (match(lexer::TokenType::)) }
+// ExprPtr Parser::conjunction() {}
+// ExprPtr Parser::equality() {}
+// ExprPtr Parser::comparison() {}
+// ExprPtr Parser::membership() {}
+// ExprPtr Parser::term() {}
+// ExprPtr Parser::factor() {}
+// ExprPtr Parser::unary() {}
+// ExprPtr Parser::postfix() {}
+// ExprPtr Parser::postfixOp() {}
+// ExprPtr Parser::primary() {}
+// ExprPtr Parser::literal() {}
+// ExprPtr Parser::list() {}
--- lang/src/parser/parser.hpp
+++ lang/src/parser/parser.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 
+#include "expression.hpp"
 #include "token.hpp"
 
 namespace invariants::parser {
@@ -10,6 +11,27 @@ class Parser {
  private:
   int curr = 0;
   std::vector<lexer::Token> tokens;
+
+  template <typename... Ts>
+    requires(std::same_as<Ts, lexer::TokenType> && ...)
+  bool match(Ts... tokens);
+
+  // Expressions
+  ast::ExprPtr expression();
+  ast::ExprPtr implication();
+  ast::ExprPtr disjunction();
+  ast::ExprPtr conjunction();
+  ast::ExprPtr equality();
+  ast::ExprPtr comparison();
+  ast::ExprPtr membership();
+  ast::ExprPtr term();
+  ast::ExprPtr factor();
+  ast::ExprPtr unary();
+  ast::ExprPtr postfix();
+  ast::ExprPtr postfixOp();
+  ast::ExprPtr primary();
+  ast::ExprPtr literal();
+  ast::ExprPtr list();
 
  public:
   explicit Parser(const std::vector<lexer::Token>& tokens);
