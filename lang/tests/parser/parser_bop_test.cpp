75d3b8b1eff0d41092a105ab05c8975800e574f2
Ujaan Das <ujaandas03@gmail.com>
2026-05-09 10:43:55 +0000

    fix test name

--- lang/tests/parser/parser_bop_test.cpp
+++ lang/tests/parser/parser_bop_test.cpp
@@ -75,4 +75,4 @@ INSTANTIATE_TEST_SUITE_P(
         TokenToBinaryOpCase{"Less", TT::LESS, BO::Less},
         TokenToBinaryOpCase{"LessEqual", TT::LESS_EQUAL, BO::LessEqual},
         TokenToBinaryOpCase{"MembershipIn", TT::KW_IN, BO::In},
-        TokenToBinaryOpCase{"MemberhsipNotIn", TT::KW_NOT_IN, BO::NotIn}));
+        TokenToBinaryOpCase{"MembershipNotIn", TT::KW_NOT_IN, BO::NotIn}));
