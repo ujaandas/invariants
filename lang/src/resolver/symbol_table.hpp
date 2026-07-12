2553be1fc8bbe73a00bc521018d1c09dcfdfc21a
Ujaan Das <ujaandas03@gmail.com>
2026-07-13 00:22:54 +0800

    add st class and reorg symbol hierarchy

--- lang/src/resolver/symbol_table.hpp
+++ lang/src/resolver/symbol_table.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 #include <string>
+#include <string_view>
+#include <unordered_map>
 
 #include "statements.hpp"
 #include "types.hpp"
@@ -11,25 +13,40 @@ namespace invariants::resolver {
 // using ModuleId = std::uint32_t; // TODO: Add importing module support later
 
 using SpecId = std::uint32_t;
-struct SpecSymbol {
-  SpecId id;
-  std::string name;
-  const ast::SpecStmt* decl;
-};
-
 using FieldId = std::uint32_t;
-struct FieldSymbol {
-  FieldId id;
-  std::string name;
-
-  ast::Type type;
-  const ast::FieldStmt* decl;
-};
-
 using ConstraintId = std::uint32_t;
+
 struct ConstraintSymbol {
   ConstraintId id;
   const ast::ConstraintStmt* decl;
+};
+
+struct FieldSymbol {
+  FieldId id;
+  std::string name;
+  ast::Type type;
+
+  const ast::FieldStmt* decl;
+
+  std::vector<ConstraintSymbol> constraints;
+};
+
+struct SpecSymbol {
+  SpecId id;
+  std::string name;
+
+  const ast::SpecStmt* decl;
+
+  std::unordered_map<std::string, FieldSymbol> fields;
+};
+
+class SymbolTable {
+ public:
+  explicit SymbolTable() = default;
+  SpecSymbol* lookup_spec(std::string_view);
+
+ private:
+  std::unordered_map<std::string, SpecSymbol> specs;
 };
 
 }  // namespace invariants::resolver
