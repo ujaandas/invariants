cc72d15916fe7864648ee656f1f61c704c37c536
Ujaan Das <ujaandas03@gmail.com>
2026-07-13 21:47:57 +0800

    add st to resolver

--- lang/src/resolver/resolver.hpp
+++ lang/src/resolver/resolver.hpp
@@ -1,34 +1,45 @@
 #pragma once
 
 #include "statements.hpp"
+#include "symbol_table.hpp"
 
 namespace invariants::resolver {
 
 class Resolver {
+ private:
+  SymbolTable table;
+  std::string currSpecName = "";
+  SpecId nextSpecId = 0;
+  FieldId nextFieldId = 0;
+
+ public:
+  Resolver() = default;
+  const SymbolTable& getSt() const { return table; }
+
   // Statements
-  void operator()(const ast::ConstraintStmt& e) const;
-  void operator()(const ast::FieldStmt& e) const;
-  void operator()(const ast::InvariantStmt&) const;
-  void operator()(const ast::SpecStmt& e) const;
-  void operator()(const ast::ModuleStmt& e) const;
+  void operator()(const ast::ConstraintStmt& e);
+  void operator()(const ast::FieldStmt& e);
+  void operator()(const ast::InvariantStmt&);
+  void operator()(const ast::SpecStmt& e);
+  void operator()(const ast::ModuleStmt& e);
 
   // Expressions
-  //   std::string operator()(const LiteralExpr& e) const;
-  //   std::string operator()(const IdentifierExpr& e) const;
-  //   std::string operator()(const ThisExpr&) const;
-  //   std::string operator()(const ListExpr& e) const;
-  //   std::string operator()(const GroupingExpr& e) const;
-  //   std::string operator()(const PostfixExpr& e) const;
-  //   std::string operator()(const MemberAccessOp& e) const;
-  //   std::string operator()(const IndexOp& e) const;
-  //   std::string operator()(const UnaryExpr& e) const;
-  //   std::string operator()(const BinaryExpr& e) const;
+  //   std::string operator()(const LiteralExpr& e);
+  //   std::string operator()(const IdentifierExpr& e);
+  //   std::string operator()(const ThisExpr&);
+  //   std::string operator()(const ListExpr& e);
+  //   std::string operator()(const GroupingExpr& e);
+  //   std::string operator()(const PostfixExpr& e);
+  //   std::string operator()(const MemberAccessOp& e);
+  //   std::string operator()(const IndexOp& e);
+  //   std::string operator()(const UnaryExpr& e);
+  //   std::string operator()(const BinaryExpr& e);
 
   // Types
-  //   std::string operator()(const BuiltinType& e) const;
-  //   std::string operator()(const SimpleType& e) const;
-  //   std::string operator()(const ArrayType&) const;
-  //   std::string operator()(const MapType& e) const;
+  //   std::string operator()(const BuiltinType& e);
+  //   std::string operator()(const SimpleType& e);
+  //   std::string operator()(const ArrayType&);
+  //   std::string operator()(const MapType& e);
 };
 
 }  // namespace invariants::resolver
