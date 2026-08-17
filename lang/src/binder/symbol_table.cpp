033fa13dd8719fcfa3536fc3cd4a929c75043da0
Ujaan Das <ujaandas03@gmail.com>
2026-08-17 10:20:51 +0000

    add binder and copy st stuff from resolver

---
+++ lang/src/binder/symbol_table.cpp
@@ -0,0 +1,54 @@
+#include "symbol_table.hpp"
+
+#include <memory>
+#include <string>
+#include <utility>
+
+using namespace invariants::resolver;
+
+const SpecSymbol* SymbolTable::lookup_spec(std::string_view spec) const {
+  auto it = specs.find(std::string(spec));
+  if (it != specs.end()) {
+    return it->second.get();
+  }
+  return nullptr;
+}
+
+const FieldSymbol* SymbolTable::lookup_field(std::string_view specName,
+                                             std::string_view fieldName) const {
+  auto foundSpec = lookup_spec(specName);
+  if (!foundSpec) {
+    return nullptr;
+  }
+
+  auto& fields = foundSpec->fields;
+  auto it = fields.find(std::string(fieldName));
+  if (it != fields.end()) {
+    return it->second.get();
+  }
+  return nullptr;
+}
+
+bool SymbolTable::add_spec(std::string_view name,
+                           std::unique_ptr<SpecSymbol> spec) {
+  if (!spec) return false;
+
+  auto [_, inserted] = specs.try_emplace(std::string(name), std::move(spec));
+  return inserted;
+}
+
+bool SymbolTable::add_field(std::string_view specName,
+                            std::string_view fieldName,
+                            std::unique_ptr<FieldSymbol> field) {
+  if (!field) return false;
+
+  auto it = specs.find(std::string(specName));
+  if (it == specs.end()) {
+    return false;
+  }
+
+  auto& fields = it->second->fields;
+  auto [_, inserted] =
+      fields.try_emplace(std::string(fieldName), std::move(field));
+  return inserted;
+}
---
+++ lang/src/binder/symbol_table.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+#include "statements.hpp"
+#include "types.hpp"
+
+namespace invariants::resolver {
+
+// using ModuleId = std::uint32_t; // TODO: Add importing module support later
+
+using SpecId = std::uint32_t;
+using FieldId = std::uint32_t;
+
+struct FieldSymbol {
+  FieldId id;
+  std::string name;
+  const ast::Type* type;
+
+  const ast::FieldStmt* decl;
+};
+
+struct SpecSymbol {
+  SpecId id;
+  std::string name;
+
+  const ast::SpecStmt* decl;
+
+  std::unordered_map<std::string, std::unique_ptr<FieldSymbol>> fields;
+};
+
+class SymbolTable {
+ public:
+  const SpecSymbol* lookup_spec(std::string_view) const;
+  const FieldSymbol* lookup_field(std::string_view, std::string_view) const;
+
+  bool add_spec(std::string_view, std::unique_ptr<SpecSymbol>);
+  bool add_field(std::string_view, std::string_view,
+                 std::unique_ptr<FieldSymbol>);
+
+ private:
+  std::unordered_map<std::string, std::unique_ptr<SpecSymbol>> specs;
+};
+
+}  // namespace invariants::resolver
