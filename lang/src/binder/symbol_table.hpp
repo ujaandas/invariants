1cb56fd7c36da9b0ae095a98d0c0a3aec46c69a1
Ujaan Das <79176268+ujaandas@users.noreply.github.com>
2026-08-25 22:34:23 +0800

    Binder (#31)
    * init dependency graph builder class/visitor
    
    * add generish-ish graph hdr
    
    * add simple graph impl
    
    * dependency graph hdr
    
    * harmonize with resolver and finish dep visitor
    
    * flip dependency edge
    
    * make resolver visitor non-const
    
    * add binder and copy st stuff from resolver
    
    * make resolver const again
    
    * remove binder.cpp from source from now
    
    * add binder tests
    
    * fix old namespace
    
    * add resolved types and more to st
    
    * use transparent hashing
    
    * rework symbol table binder impl
    
    * add new tests and tweak odr
    
    * add bound expr
    
    * bound tree stmt nodes too
    
    * add map n array types
    
    * add map array type tests
    
    * add binder
    
    * add nullptr literal and binder impl
    
    * add passing tests

---
+++ lang/src/binder/symbol_table.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <variant>
+
+#include "statements.hpp"
+#include "types.hpp"
+
+struct TransparentStringHash {
+  using is_transparent = void;
+
+  std::size_t operator()(std::string_view sv) const noexcept {
+    return std::hash<std::string_view>{}(sv);
+  }
+};
+
+namespace invariants::binder {
+
+// using ModuleId = std::uint32_t; // TODO: Add importing module support later
+
+using SpecId = std::uint32_t;
+using FieldId = std::uint32_t;
+
+struct SpecSymbol;
+struct ResolvedArrayType;
+struct ResolvedMapType;
+
+using TypeVar = std::variant<ast::BuiltinType, const SpecSymbol*,
+                             std::shared_ptr<ResolvedArrayType>,
+                             std::shared_ptr<ResolvedMapType>>;
+
+struct ResolvedType {
+  TypeVar type;
+  bool isBuiltin() const;
+  bool isCustom() const;
+  bool isArray() const;
+  bool isMap() const;
+};
+
+struct ResolvedArrayType {
+  ResolvedType element;
+};
+
+struct ResolvedMapType {
+  ResolvedType key;
+  ResolvedType value;
+};
+
+struct FieldSymbol {
+  FieldId id;
+  std::string name;
+  ResolvedType resType;
+};
+
+struct SpecSymbol {
+  SpecId id;
+  std::string name;
+  std::unordered_map<std::string, std::unique_ptr<FieldSymbol>,
+                     TransparentStringHash, std::equal_to<>>
+      fields;
+};
+
+class SymbolTable {
+ public:
+  const SpecSymbol* lookup_spec(std::string_view) const;
+  const FieldSymbol* lookup_field(std::string_view, std::string_view) const;
+
+  SpecSymbol* add_spec(std::string_view, const ast::SpecStmt*);
+  FieldSymbol* add_field(std::string_view, std::string_view, ResolvedType,
+                         const ast::FieldStmt*);
+
+  std::size_t get_total_field_count() const;
+
+ private:
+  SpecId nextSpecId = 0;
+  FieldId nextFieldId = 0;
+
+  std::unordered_map<std::string, std::unique_ptr<SpecSymbol>,
+                     TransparentStringHash, std::equal_to<>>
+      specs;
+};
+
+}  // namespace invariants::binder
