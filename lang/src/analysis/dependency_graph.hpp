b5acdb774771b88820f8d7618ed3f52d96254c28
Ujaan Das <ujaandas03@gmail.com>
2026-08-27 13:16:43 +0000

    wip dep graph outline

--- lang/src/analysis/dependency_graph.hpp
+++ lang/src/analysis/dependency_graph.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
 
 #include "symbol_table.hpp"
 
@@ -12,6 +13,24 @@ class DependencyGraph {
 
   // Add dependency where from is gen before to
   void addEdge(binder::FieldId, binder::FieldId);
+
+  bool hasEdge(binder::FieldId, binder::FieldId) const;
+  std::size_t size() const;
+
+  // Returns all dependencies of field
+  const std::vector<binder::FieldId>& getDependencies(binder::FieldId) const;
+
+  // Returns all dependents (fields that depend on) of field
+  const std::vector<binder::FieldId>& getDependents(binder::FieldId) const;
+
+  // Compute gen order
+  std::vector<binder::FieldId> order() const;
+
+ private:
+  std::size_t numFields;
+  std::vector<std::vector<binder::FieldId>> outgoingAdj;
+  std::vector<std::vector<binder::FieldId>> incomingAdj;
+  std::vector<std::size_t> inDeg;
 };
 
 }  // namespace invariants::analysis
