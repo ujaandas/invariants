fabf97c055b4e4f4d09c8094694cccecd723634b
Ujaan Das <79176268+ujaandas@users.noreply.github.com>
2026-08-31 15:30:18 +0800

    Evaluator (#40)
    * add runtime possible types
    
    * deprecate old and add eval blueprint
    
    * wip evaluator
    
    * bound unary expr evaluator
    
    * binary expr all other
    
    * deprecate bindings and old runtime
    
    * initial evaluator testing
    
    * add more evaluator tests
    
    * more exhaustive tests for all operators

---
+++ lang/src/runtime/evaluator.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "bound_expr.hpp"
+#include "types.hpp"
+
+namespace invariants::runtime {
+
+class Evaluator {
+ private:
+  const Environment* currentEnv = nullptr;
+
+ public:
+  Value evaluate(const binder::BoundExpr& expr, const Environment& env);
+
+  Value operator()(const binder::BoundLiteralExpr& expr) const;
+  Value operator()(const binder::BoundFieldAccessExpr& expr) const;
+  Value operator()(const binder::BoundBinaryExpr& expr) const;
+  Value operator()(const binder::BoundUnaryExpr& expr) const;
+  Value operator()(const binder::BoundListExpr& expr) const;
+  Value operator()(const binder::BoundValueAccessExpr& expr) const;
+};
+
+}  // namespace invariants::runtime
