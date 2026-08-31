f6b60793e8772a9cb92c2df581f202bde624a09d
Ujaan Das <79176268+ujaandas@users.noreply.github.com>
2026-08-31 15:18:08 +0800

    Environment evaluator (#38)
    * add runtime possible types
    
    * deprecate old and add eval blueprint
    
    * wip evaluator
    
    * bound unary expr evaluator
    
    * binary expr all other
    
    * deprecate bindings and old runtime
    
    * initial evaluator testing
    
    * add more evaluator tests

--- lang/src/runtime/runtime.hpp
+++ lang/src/runtime/runtime.hpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <string_view>
 #include <variant>
-namespace invariants::runtime {
+namespace invariants::runtime::deprecated {
 
 // Keep these simple for now
 enum class FieldType : std::uint8_t { Integer, Number, String };
@@ -75,4 +75,4 @@ class Runtime {
   void reset();
 };
 
-}  // namespace invariants::runtime
+}  // namespace invariants::runtime::deprecated
